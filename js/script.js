f00d07cc50b3db834e76891e689930c009ec7486
Beatrice Yap <beatriceyapsm@gmail.com>
2022-09-16 15:59:35 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Buttons
+function addaccountButton() {
+    location.href = "/addaccount"
+}
+function monthlycontButton() {
+    location.href = "/monthlycont"
+}
+function dashboardButton() {
+    location.href = "/dashboard"
+}
+
+function savehouseButton() {
+    location.href = "/savehouse"
+}
+
+//Redirect click on icon to welcome page
+function homeIconClick() {
+    location.href = "/welcome"
+}
