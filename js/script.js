ba31ef6c3182264dbbc38824119772df6bbc7040
Beatrice Yap <beatriceyapsm@gmail.com>
2022-09-16 16:08:19 +0800

    edits

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 //Buttons
 function addaccountButton() {
-    location.href = "/addaccount"
+    location.href = "/addaccount.html"
 }
 function monthlycontButton() {
-    location.href = "/monthlycont"
+    location.href = "/monthlycont.html"
 }
 function dashboardButton() {
-    location.href = "/dashboard"
+    location.href = "/dashboard.html"
 }
 
 function savehouseButton() {
-    location.href = "/savehouse"
+    location.href = "/savehouse.html"
 }
 
 //Redirect click on icon to welcome page
 function homeIconClick() {
-    location.href = "/welcome"
+    location.href = "/welcome.html"
 }
