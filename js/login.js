c2627a0efd7561402d4de018b61cbe921c40c0b1
Nithin <nithin.a7@gmail.com>
2022-09-20 01:50:25 +0800

    updated login

--- js/login.js
+++ js/login.js
@@ -7,16 +7,23 @@ const b1 = document.getElementById('b1');
 function getFromServer(e) {
   e.preventDefault();
   var querryStr = "https://nus-money.herokuapp.com/login?email=" + uid.value + "&password=" + pass.value;
-  console.log(querryStr);
+  var querryStr2 = "https://nus-money.herokuapp.com/user?Email=" + uid.value;
+  // console.log(querryStr);
   //USING FETCH instead of Jquery
   fetch(querryStr)
     // when we get a response map the body to json
     .then((response) => response.json())
     // and pass the JSON data to mydata for rendering
-    .then((data) => renderhtml(data));
+    .then((data) => loginValidation(data));
+
+  fetch(querryStr2)
+      // when we get a response map the body to json
+      .then((response) => response.json())
+      // and pass the JSON data to mydata for rendering
+      .then((data2) => localStorage.setItem('FirstName',JSON.parse(JSON.stringify(data2[0])).FirstName));
 }
 
-function renderhtml(data) {
+function loginValidation(data) {
   console.log(data.success);
   if (data.success == true) {
     //Storing data to local storage for the session
@@ -30,6 +37,7 @@ function renderhtml(data) {
     document.querySelector(".mypanel").innerHTML = text;
   }
 }
+
 
 b1.addEventListener('click', getFromServer);
 
